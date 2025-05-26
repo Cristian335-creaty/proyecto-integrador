26065a17b42259ae3117fdd2f906d92a81b5842d
Cesar Ivan Moreno Acero <cesar.morenoac@usantoto.edu.co>
2025-05-25 23:14:50 -0500

    Agregar archivos de proyecto.

---
+++ Docentes2/Services/IContenidoService.cs
@@ -0,0 +1,13 @@
+﻿using Docentes2.Models;
+
+namespace Docentes2.Services
+{
+    public interface IContenidoService
+    {
+        Task<List<Contenido>> GetAllAsync();
+        Task<Contenido> GetByIdAsync(int id);
+        Task CreateAsync(Contenido contenido);
+        Task UpdateAsync(Contenido contenido);
+        Task DeleteAsync(int id);
+    }
+}
